e75f07b50eba03666be340bfaeea15a92239d1e7
Shynih <jorgepeng2001@gmail.com>
2020-12-01 20:19:25 -0800

    update

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main(int argc, char** argv) { 
    Base* parse;
    parse = parse -> parse(argv, argc);
-   cout << parse -> stringify();
+   cout << parse -> stringify() << " = " << parse -> evaluate() << endl;
 
    return 0; 
 } 
