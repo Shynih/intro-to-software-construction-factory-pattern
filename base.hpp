cc1460f743a705f0c34e5c6109167f4e183b650c
Shynih <jorgepeng2001@gmail.com>
2020-12-01 18:40:15 -0800

    Added parse

--- base.hpp
+++ base.hpp
@@ -11,6 +11,40 @@ class Base {
         /* Pure Virtual Functions */
         virtual double evaluate() = 0;
         virtual std::string stringify() = 0;
+        Base* parse(char** input, int length) {
+            Base* firstVal = new Op(input[1][0] + 48);
+            for (int i = 2; i < length; i++) {
+                if (input[i] == "*") {
+                    Base* anotherVal = new Op(input[i+1][0] + 48);
+                    firstVal = new Mult(firstVal, anotherVal);
+                    i++;
+                }
+                else if (input[i] == "**") {
+                    Base* anotherVal = new Op(input[i+1][0] + 48);
+                    firstVal = new Pow(firstVal, anotherVal);
+                    i++;
+                }
+                else if (input[i] == "/") {
+                    Base* anotherVal = new Op(input[i+1][0] + 48);
+                    firstVal = new Div(firstVal, anotherVal);
+                    i++;
+                }
+                else if (input[i] == "+") {
+                    Base* anotherVal = new Op(input[i+1][0] + 48);
+                    firstVal = new Add(firstVal, anotherVal);
+                    i++;
+                }
+                else if (input[i] == "-") {
+                    Base* anotherVal = new Op(input[i+1][0] + 48);
+                    firstVal = new Sub(firstVal, anotherVal);
+                    i++;
+                }
+                else {
+                    Base* firstVal = nullptr;
+                }
+            }
+            return firstVal;
+        }
 };
 
 #endif //__BASE_HPP__
